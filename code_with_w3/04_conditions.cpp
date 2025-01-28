6352cb352e3c10384a4417d2fcea0ebecaa3cead
biplob-kb <biplobkb123@gmail.com>
2025-01-28 12:44:54 +0600

    completed the conditions

--- code_with_w3/04_conditions.cpp
+++ code_with_w3/04_conditions.cpp
@@ -27,5 +27,27 @@ int main()
 
     */
 
+    //----------------------------Finding big number from three-------
+    int firstNum, secondNum, thirdNum;
+    cout << "Enter first number : ";
+    cin >> firstNum;
+    cout << "Enter second number : ";
+    cin >> secondNum;
+    cout << "Enter third number : ";
+    cin >> thirdNum;
+
+    if (firstNum > secondNum && firstNum > thirdNum)
+    {
+        cout << "The big number is : " << firstNum << endl;
+    }
+    else if (secondNum > firstNum && secondNum > thirdNum)
+    {
+        cout << "The big number is : " << secondNum << endl;
+    }
+    else
+    {
+        cout << "The big number is : " << thirdNum << endl;
+    }
+
     return 0;
 }
