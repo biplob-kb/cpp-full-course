637e0dc187cbdfc310706df28e0a649ccca1e1a7
biplob-kb <biplobkb123@gmail.com>
2025-01-28 12:10:50 +0600

    updated the string

--- code_with_w3/03_string.cpp
+++ code_with_w3/03_string.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
+
+    //-----------------------------character conversion----------------------
+    /*
     char ch;
-    cout << "Enter a Lowercase letter : ";
+    cout << "Enter a letter : ";
     cin >> ch;
 
     if (ch >= 'a' && ch <= 'z')
@@ -12,10 +15,28 @@ int main()
         char uppercaseChar = ch - 32;
         cout << "Uppercase is : " << uppercaseChar << endl;
     }
+    else if (ch >= 'A' && ch <= 'Z')
+    {
+        char lowercaseChar = ch + 32;
+        cout << "Lowercase is : " << lowercaseChar << endl;
+    }
     else
     {
         cout << "Your input is wrong" << endl;
     }
+    */
+
+    //------------------------------string operations-------------------------------------
+    string text;
+    cout << "Enter a word : ";
+    getline(cin, text);
+
+    cout << "You have entered : " << text << endl;
+    cout << "Length of the word is : " << text.length() << endl;
+    cout << "First character is : " << text[0] << endl;
+    cout << "Last character is : " << text[text.length() - 1] << endl;
+    text[0] = 'J';
+    cout << "First character changed by J : " << text << endl;
 
     return 0;
 }
