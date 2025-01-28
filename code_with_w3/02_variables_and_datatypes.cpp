f64f9a628559692b1a88e7686d44ed241c0f5b70
biplob-kb <biplobkb123@gmail.com>
2025-01-28 10:25:27 +0600

    completed the variable and datatypes

--- code_with_w3/02_variables_and_datatypes.cpp
+++ code_with_w3/02_variables_and_datatypes.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Now it is time to play with variable and datatypes";
+    string productName;
+    float price;
+    int quantity;
+    float sellPrice;
+
+    cout << "Enter product name : ";
+    getline(cin, productName);
+
+    cout << "Enter per product price : ";
+    cin >> price;
+    cout << "Enter product quantity : ";
+    cin >> quantity;
+
+    cout << "Product name : " << productName << endl;
+    cout << "Per product price : " << price << endl;
+    cout << "Product quantity : " << quantity << endl;
+    cout << "Total Cost : " << price * quantity << endl;
+
+    cout << "----------------------------" << endl;
+    cout << "Price per product for sell : ";
+    cin >> sellPrice;
+
+    cout << "Your profit : " << (sellPrice * quantity) - (price * quantity) << endl;
     cout << endl;
     return 0;
 }
