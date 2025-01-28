3778563c39de48e93655304faf513377a4fac1eb
biplob-kb <biplobkb123@gmail.com>
2025-01-28 14:18:12 +0600

    star printing is initialize

---
+++ code_with_w3/07_star_printing.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+void print_square_shape(int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void print_triangle_shape1(int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (j <= i)
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void print_triangle_shape2(int row, int col)
+{
+    for (int i = row; i > 0; i--)
+    {
+        for (int j = 1; j <= row; j++)
+        {
+            if (j < i)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int row, col;
+    cout << "Enter row : ";
+    cin >> row;
+    cout << "Enter col : ";
+    cin >> col;
+
+    print_square_shape(row, col);
+    print_triangle_shape1(row, col);
+    print_triangle_shape2(row, col);
+    return 0;
+}
