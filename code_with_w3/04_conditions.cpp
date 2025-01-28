eb8201f38314355bfc4360e3e3d1ffd61a6934cd
biplob-kb <biplobkb123@gmail.com>
2025-01-28 12:32:05 +0600

    conditions is now comming

---
+++ code_with_w3/04_conditions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //-------------------------Leap year-------------------------
+    /*
+    int year;
+    cout << "Enter a year : ";
+    cin >> year;
+
+    if (year % 400 == 0)
+    {
+        cout << "It is Leap year." << endl;
+    }
+    else
+    {
+        if (year % 100 != 0 && year % 4 == 0)
+        {
+            cout << "It is Leap year." << endl;
+        }
+        else
+        {
+            cout << "It is not Leap year." << endl;
+        }
+    }
+
+    */
+
+    return 0;
+}
