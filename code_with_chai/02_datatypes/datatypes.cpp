48b50f5b22380b39e6115ddf2ff6354e63648755
biplob-kb <biplobkb123@gmail.com>
2025-01-29 12:24:47 +0600

    created the folder and file for the data types

---
+++ code_with_chai/02_datatypes/datatypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float myBalance;
+    string accountNo;
+    int my age;
+
+    return 0;
+}
---
+++ code_with_chai/02_datatypes/string.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Here We create the string" << endl;
+    return 0;
+}
